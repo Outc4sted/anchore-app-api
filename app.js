7d4bc516e1e80427186e27ee4000cca37959261b
Justen <outc4sted@gmail.com>
2021-03-28 15:36:42 -0700

    some cleanup

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
 
@@ -14,8 +12,6 @@ app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
